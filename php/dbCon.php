610625d4b70a1d425ff48898e118648b5be10c3b
sohaib <sohaibnasser@gmail.com>
2020-11-20 15:33:35 +0500

    commit

--- php/dbCon.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*$link = mysql_pconnect("localhost:3306", "mzdataso_website", "ScbAbbas@123$") or die("Unable To Connect To Database Server");
-mysql_select_db("mzdataso_website") or die("Unable To Connect To DB");*/
-
-$link = mysql_pconnect("localhost:3306", "mzds", "mzds") or die("Unable To Connect To Database Server");
-mysql_select_db("mzds") or die("Unable To Connect To DB");
-
-?>
